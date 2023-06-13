481aed2b4c371a6d87e78bbdeb60bb70f1f87105
Nobuya WATANABE <nobuya@okayama-u.ac.jp>
2023-06-13 22:18:18 +0900

    add collision with bricks

--- main.js
+++ main.js
@@ -49,7 +49,7 @@ const bricks = [];
 for (let c = 0; c < brickColumnCount; c++) {
     bricks[c] = [];
     for (let r = 0; r < brickRowCount; r++) {
-	bricks[c][r] = { x: 0, y: 0 };
+	bricks[c][r] = { x: 0, y: 0, status: 1 };
     }
 }
 
@@ -74,15 +74,17 @@ function drawPaddle() {
 function drawBricks() {
     for (let c = 0; c < brickColumnCount; c++) {
         for (let r = 0; r < brickRowCount; r++) {
-            const brickX = c * (brickWidth + brickPadding) + brickOffsetLeft;
-            const brickY = r * (brickHeight + brickPadding) + brickOffsetTop;
-            bricks[c][r].x = brickX;
-            bricks[c][r].y = brickY;
-            ctx.beginPath();
-            ctx.rect(brickX, brickY, brickWidth, brickHeight);
-            ctx.fillStyle = "#0095DD";
-            ctx.fill();
-            ctx.closePath();
+	    if (bricks[c][r].status === 1) { // do drawing?
+		const brickX = c * (brickWidth + brickPadding) + brickOffsetLeft;
+		const brickY = r * (brickHeight + brickPadding) + brickOffsetTop;
+		bricks[c][r].x = brickX;
+		bricks[c][r].y = brickY;
+		ctx.beginPath();
+		ctx.rect(brickX, brickY, brickWidth, brickHeight);
+		ctx.fillStyle = "#0095DD";
+		ctx.fill();
+		ctx.closePath();
+	    } // if (bricks[c][r].status === 1) 
         }
     }
 } // function drawBricks()
@@ -94,6 +96,8 @@ function draw() {
     drawBall();
     // draw a paddle
     drawPaddle();
+    // collision detection between the ball and blicks
+    collisionDetection();
     // draw bricks
     drawBricks();
       
@@ -168,6 +172,22 @@ function keyUpHandler(e) {
 	leftPressed = false;
     }
 } // function keyUpHandler(e)
+
+function collisionDetection() {
+    for (let c = 0; c < brickColumnCount; c++) {
+        for (let r = 0; r < brickRowCount; r++) {
+            const b = bricks[c][r];
+            if (b.status === 1) { // does the brick exist?
+                if (x > b.x && x < b.x+brickWidth &&
+                    y > b.y && y < b.y+brickHeight) {
+                    dy = -dy;
+                    b.status = 0; // disappear
+                }
+            }
+        }
+    }
+} // function collisionDetection()
+
 
 // draw interval
 const interval = setInterval(draw, 20);
